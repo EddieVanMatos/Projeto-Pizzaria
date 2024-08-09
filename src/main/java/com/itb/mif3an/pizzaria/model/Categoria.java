57fa59de32762f3634da18560e7c76bd9f1e1fc9
Edivan Matos <eddievan.matos@gmail.com>
2024-08-08 22:44:54 -0300

    Configurando o Id generatedValue IDENTITY

--- src/main/java/com/itb/mif3an/pizzaria/model/Categoria.java
+++ src/main/java/com/itb/mif3an/pizzaria/model/Categoria.java
@@ -1,7 +1,12 @@
 package com.itb.mif3an.pizzaria.model;
 
-public class Categoria {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
